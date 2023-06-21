09172157918344b403112a17424aa7b22f6f2036
BlazeCSS <134992343+blazecss@users.noreply.github.com>
2023-06-21 09:29:02 -0600

    0.0.2

--- maraconi.js
+++ maraconi.js
@@ -3,3 +3,19 @@ window.onmousemove = (e) => {
     pointer.style.top=e.clientY;
     pointer.style.left=e.clientX;
 }
+
+let calc=document.getElementById("calculatorapp");
+let wallpaper=document.getElementById("wallpaperapp");
+let hoverable_elements=[calc, wallpaper];
+for (let i=0; i<hoverable_elements.length; i++) {
+    hoverable_elements[i].onmouseover = (e) => {
+        pointer.setAttribute("src", "macpointer.png");
+    }
+    hoverable_elements[i].onmouseout = (e) => {
+        pointer.setAttribute("src", "maccursorpointer.png");
+    }
+}
+
+wallpaper.addEventListener("click", () => {
+    
+});
